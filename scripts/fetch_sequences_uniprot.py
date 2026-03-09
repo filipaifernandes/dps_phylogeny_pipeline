0c20fa7c3e2713ec4d78e3f24ba13c3942e763c4
Filipa Fernandes <filipaifernandes.2005@gmail.com>
2026-03-09 11:01:55 +0000

    changing the way to fetch the sequences

--- scripts/fetch_sequences_uniprot.py
+++ scripts/fetch_sequences_uniprot.py
@@ -1,34 +1,32 @@
 """A script to fetch the desired protein from UniProt database using REST API"""
-#!/usr/bin/env python3
-#!/usr/bin/env python3
 import requests
+from Bio import SeqIO
+from io import StringIO
 import sys
-import urllib.parse
 
-if len(sys.argv) != 4:
-    sys.exit(
-        "ERROR: Expected 3 arguments:\n"
-        "fetch_sequences_uniprot.py <gene> <taxon> <output_file>")
+gene = sys.argv[1]
+taxon = sys.argv[2]
+output = sys.argv[3]
 
-gene, taxon, output_file = sys.argv[1:]
+query = f"gene:{gene} AND taxonomy_id:{taxon}"
 
-#query
-query = f"gene_exact:{gene} AND taxonomy_id:{taxon}"
+url = "https://rest.uniprot.org/uniprotkb/search"
 
-url = "https://rest.uniprot.org/uniprotkb/stream"
+params = {
+    "query": query,
+    "format": "fasta",
+    "size": 500
+}
 
-params = {"query": query, "format": "fasta"}
+response = requests.get(url, params=params)
 
-try:
-    response = requests.get(url, params=params, timeout=30)
-    response.raise_for_status()
-except requests.exceptions.RequestException as e:
-    sys.exit(f"ERROR: UniProt request failed: {e}")
+if response.status_code != 200:
+    raise Exception("UniProt request failed")
 
-if not response.text.strip():
-    print(f"WARNING: No UniProt sequences found for gene='{gene}' and taxon='{taxon}'.")
-    open(output_file, "w").close()
-else:
-    with open(output_file, "w") as out_fasta:
-        out_fasta.write(response.text)
-    print(f"Successfully retrieved UniProt sequences for gene='{gene}' and taxon='{taxon}'.")
+fasta_data = response.text
+
+records = list(SeqIO.parse(StringIO(fasta_data), "fasta"))
+
+SeqIO.write(records, output, "fasta")
+
+print(f"Downloaded {len(records)} sequences")
