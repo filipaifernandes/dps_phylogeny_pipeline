ba479c00f35afd61b3d9951f37be8aed17adf05d
Filipa Fernandes <filipaifernandes.2005@gmail.com>
2026-03-10 10:48:14 +0000

    improving the script

--- scripts/merge_clean_fasta.py
+++ scripts/merge_clean_fasta.py
@@ -1,16 +1,35 @@
 """A script to merge FASTA files and clean them"""
 from Bio import SeqIO
 import sys
+import re
 
 inputs = sys.argv[1:-1]
 output = sys.argv[-1]
 
 seen = set()
 
+MIN_LEN = 150
+MAX_LEN = 300
+
+valid_aa = re.compile("^[ACDEFGHIKLMNPQRSTVWY]+$")
+
 with open(output, "w") as out:
     for fasta in inputs:
         for record in SeqIO.parse(fasta, "fasta"):
-            seq = str(record.seq)
-            if seq not in seen:
-                seen.add(seq)
-                SeqIO.write(record, out, "fasta")
+
+            seq = str(record.seq).upper()
+
+            # Remove duplicates
+            if seq in seen:
+                continue
+
+            # Remove sequences with ambiguous residues
+            if not valid_aa.match(seq):
+                continue
+
+            # Length filtering
+            if len(seq) < MIN_LEN or len(seq) > MAX_LEN:
+                continue
+
+            seen.add(seq)
+            SeqIO.write(record, out, "fasta")
