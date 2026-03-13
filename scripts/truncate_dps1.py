f2e4c873dc482820d8d7c38de3fcd7234e908acf
Filipa Fernandes <filipaifernandes.2005@gmail.com>
2026-03-13 10:45:31 +0000

    automating the script

--- scripts/truncate_dps1.py
+++ scripts/truncate_dps1.py
@@ -3,11 +3,13 @@ from Bio import SeqIO
 
 input_file = sys.argv[1]
 output_file = sys.argv[2]
+trunc_start = int(sys.argv[3])
+trunc_end = int(sys.argv[4])
 
 truncated_records = []
 
 for i, record in enumerate(SeqIO.parse(input_file, "fasta"), start=1):
-    trunc_seq = record.seq[53:207]  # 54–207
+    trunc_seq = record.seq[trunc_start -1 :trunc_end - 1]  # 54–207 for my specific case
     record.seq = trunc_seq
     record.id = f"{record.id}_trunc"
     record.description = ""
